dfa284f9d78ed21582f60162da2b9b272a7617bb
Zhu,Shanbo <zhushanbo@baidu.com>
2019-12-15 01:19:48 +0800

    no message

--- update.sh
+++ update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# 用法：./update.sh 3.18.22.70 192.168.0.111
 # Mac系统下，查找含有$1文本的文件，并将这些文件中的$1文本替换为$2文本
 
 if [ ! $# -eq 2 -a ! $# -eq 3 ]; then
     echo usage:{$0 text1 text2 type}; exit 
 else
-    echo 正在使用文本\"$2\"替换文本\"$1\" 
+    echo 正在把文本\"$1\"替换为文本\"$2\" 
 fi
 
-if [ ! $3 ]; then
+if [ ! -z $3 ]; then
     LC_CTYPE=C sed -i "" "s/$1/$2/g" `grep -rl $1 . --include=*.$3`
 else
     LC_CTYPE=C sed -i "" "s/$1/$2/g" `grep -rl $1 .`
