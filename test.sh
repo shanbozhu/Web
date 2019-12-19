8b5e2406de7d11eff4634249612befd5e723c993
Zhu,Shanbo <zhushanbo@baidu.com>
2019-12-19 11:32:34 +0800

    no message

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ bb=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.1
 bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
 echo "$aa"
 echo "$bb"
-bb=${bb#*\n}
+bb=$(echo "$bb" | sed -n '1p')
 echo "${bb}"
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g')
 #cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
-echo "$cc"
-#./update.sh $bb $aa html
-#./update.sh $bb $aa plist 
+#echo "$cc"
+./update.sh $bb $aa html
+./update.sh $bb $aa plist 
