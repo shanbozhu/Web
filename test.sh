03ee6c69128f0d797cc7a740acb0c9962c9a5c14
Zhu,Shanbo <zhushanbo@baidu.com>
2019-12-19 11:35:18 +0800

    no message

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 172.18.22)
 bb=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
 bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
-echo "$aa"
-echo "$bb"
+#echo "$aa"
+#echo "$bb"
 bb=$(echo "$bb" | sed -n '1p')
-echo "${bb}"
+#echo "${bb}"
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
-#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g')
+#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g') # GNU版本写法,非Mac OS的BSD版本写法
 #cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
 #echo "$cc"
 ./update.sh $bb $aa html
