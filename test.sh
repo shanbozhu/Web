9512a3189bc8567664fbf4d2a780206f17a3c4a1
Zhu,Shanbo <zhushanbo@baidu.com>
2019-12-19 11:13:04 +0800

    no message

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 172.18.22)
+bb=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
+bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
+echo "$aa"
+echo "$bb"
+bb=${bb#*\n}
+echo "${bb}"
+#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
+#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g')
+#cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
+echo "$cc"
+#./update.sh $bb $aa html
+#./update.sh $bb $aa plist 
