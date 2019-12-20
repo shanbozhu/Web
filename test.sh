cf0951d226ad0f300b7c241b590608c62859aa21
zhushanbo <zhushanbo@baidu.com>
2019-12-20 19:14:42 +0800

    修改为本机ip

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # 将ip地址替换为本机地址
 
-aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 172.18.22)
-bb=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
-bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
+aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 172 | sed -n '1p')
+#bb=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g' | grep -i 172.18.22)
+bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
 #echo "$aa"
 #echo "$bb"
-bb=$(echo "$bb" | sed -n '1p')
-#echo "${bb}"
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g') # GNU版本写法,非Mac OS的BSD版本写法
 #cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
