3a08a8e1d2d459d03676d4f245b592ce12c329fc
zhushanbo <zhushanbo@baidu.com>
2019-12-20 21:11:27 +0800

    修改为本机ip

--- test.sh
+++ test.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 # 将ip地址替换为本机地址
 
-NET=172
+# 网络ip
 aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
-bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i $NET | sed -n '1p')
 echo "$aa"
+
+# index.html中的ip
+bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
+if [ -z "$bb" ]; then
+    read bb 0< lastip.conf
+    echo "-------"
+fi
 echo "$bb"
+
+
+
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
 #cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g') # GNU版本写法,非Mac OS的BSD版本写法
 #cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
 #echo "$cc"
 
 if [ -z "$aa" ] || [ -z "$bb" ]; then
-    echo "aa 或者 bb为空"
+    echo "aa或bb为空"
     exit 0
 fi
 
 ./update.sh $bb $aa html
-./update.sh $bb $aa plist 
+./update.sh $bb $aa plist
+
+# 替换完成,记录上一次的ip
+echo "$aa" 1> lastip.conf
 
 git add .
 git commit -m "修改为本机ip" &> /dev/null
