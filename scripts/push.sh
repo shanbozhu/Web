51b669f19996e808ac0cfc33d7bc26eca3d92d39
zhushanbo <zhushanbo@baidu.com>
2019-12-31 20:48:07 +0800

    no message

---
+++ scripts/push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 将改动推送到github仓库
+function pushGithub() {
+    echo "$1"
+    
+    git add .
+    git commit -m "修改为本机ip" &> /dev/null
+    git push &> /dev/null
+    return 0
+}
+
+# 将manifest.plist文件推送到coding仓库
+function pushCoding() {
+    echo "$1"
+    
+    cp -a ./common/manifest.plist ../OTA
+    cd ../OTA
+    
+    git add .
+    git commit -m "上传manifest文件" &> /dev/null
+    git push &> /dev/null
+    return 0
+}
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # 将当前目录下所有html和plist文件中的ip地址替换为本机地址
 
+directory=../
+htmlPath=${directory}index.html
+lastIPPath=./lastip.conf
+
 # 获取网络ip
 function getNetIP() {
     netIP=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
@@ -12,9 +16,9 @@ echo "当前网络的ip:$netIP"
 
 # 获取index.html中的ip
 function getLocalFileIP() {
-    localFileIP=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
+    localFileIP=$(cat ${htmlPath} | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
     if [ -z "$localFileIP" ]; then
-        read localFileIP 0< lastip.conf
+        read localFileIP 0< $lastIPPath
     fi
     echo "$localFileIP"
     return 0;
@@ -44,7 +48,7 @@ replace
 URL="http://$netIP"
 function afterReplacement() {
     # 记录上一次的ip到本地文件
-    echo "$netIP" 1> lastip.conf
+    echo "$netIP" 1> $lastIPPath
 
     # 生成访问本站地址的二维码
     qrencode -o index.png -s 10 -m 1 "$URL"
