fb3fe41bf999de9d4ff965d055c286327f800ddf
zhushanbo <zhushanbo@baidu.com>
2019-12-31 16:57:53 +0800

    no message

--- test.sh
+++ test.sh
@@ -2,15 +2,23 @@
 # 将ip地址替换为本机地址
 
 # 获取网络ip
-aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
-echo "当前网络的ip:$aa"
+function getNetIP() {
+    aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
+    echo "当前网络的ip:$aa"
+    return 0;
+}
+getNetIP
 
 # 获取index.html中的ip
-bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
-if [ -z "$bb" ]; then
-    read bb 0< lastip.conf
-fi
-echo "本地文件的ip:$bb"
+function getLocalFileIP() {
+    bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
+    if [ -z "$bb" ]; then
+        read bb 0< lastip.conf
+    fi
+    echo "本地文件的ip:$bb"
+    return 0;
+}
+getLocalFileIP
 
 if [ -z "$aa" ] || [ -z "$bb" ] || [ "$aa" == "$bb" ]; then
     echo "替换失败!获取到的ip无效或二者相同"
