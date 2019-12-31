ea48330bde4cc8918093e2e6cb03a4afd942c567
zhushanbo <zhushanbo@baidu.com>
2019-12-31 20:08:33 +0800

    修改为本机ip

--- test.sh
+++ test.sh
@@ -53,7 +53,7 @@ afterReplacement
 
 source push.sh
 function push() {
-    pushGithub 正在推送本地改动到github仓库...
+    pushGithub 正在推送本地改动到github仓库 ...
     pushCoding "正在推送manifest.plist文件到coding仓库..."
     return 0
 }
