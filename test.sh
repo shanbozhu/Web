07567f6e110fda3e69187d1a5236fd1022463064
zhushanbo <zhushanbo@baidu.com>
2019-12-31 14:47:10 +0800

    no message

--- test.sh
+++ test.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 # 将ip地址替换为本机地址
 
-# 网络ip
+# 获取网络ip
 aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
-echo "$aa"
+echo "当前网络的ip:$aa"
 
-# index.html中的ip
+# 获取index.html中的ip
 bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
-echo "$bb"
 if [ -z "$bb" ]; then
     read bb 0< lastip.conf
-    echo "-------"
 fi
-echo "$bb"
-
-
-
-#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\'$'\n/g')
-#cc=$(cat ~/Desktop/CustomWeb/index.html | sed -e 's/\//\n/g') # GNU版本写法,非Mac OS的BSD版本写法
-#cc=$(more ~/Desktop/CustomWeb/index.html | tr '\/' '\n')
-#echo "$cc"
+echo "本地文件的ip:$bb"
 
 if [ -z "$aa" ] || [ -z "$bb" ] || [ "$aa" == "$bb" ]; then
-    echo "aa或bb为空或aa与bb相同"
+    echo "替换失败!获取到的ip无效或二者相同"
     exit 0
 fi
 
+# 调用update.sh脚本执行替换操作
 ./update.sh $bb $aa html
 ./update.sh $bb $aa plist
 
 # 替换完成,记录上一次的ip
 echo "$aa" 1> lastip.conf
 
+# 输出成功提示
 URL="http://$aa"
-#open URL
 echo -e "🍺 浏览器访问: \033[34m$URL\033[0m"
+
+# 生成访问本站地址的二维码
 qrencode -o index.png -s 10 -m 1 "$URL"
 
+# 将改动推送到github仓库
 git add .
 git commit -m "修改为本机ip" &> /dev/null
 git push &> /dev/null
 
+# 将manifest.plist文件推送到coding仓库
 cp -a ./common/manifest.plist ../OTA
 cd ../OTA
 git add .
