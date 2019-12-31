a53e6de448a84491a56141382fd5827eb93122af
zhushanbo <zhushanbo@baidu.com>
2019-12-31 17:12:10 +0800

    no message

--- test.sh
+++ test.sh
@@ -3,37 +3,41 @@
 
 # 获取网络ip
 function getNetIP() {
-    aa=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
-    echo "当前网络的ip:$aa"
+    NETIP=$(ifconfig en0 | sed -e 's/ /\'$'\n/g' | grep -i 1 | sed -n '3p')
+    echo "$NETIP"
     return 0;
 }
-getNetIP
+NETIP=$(getNetIP)
+echo "当前网络的ip:$NETIP"
 
 # 获取index.html中的ip
 function getLocalFileIP() {
-    bb=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
-    if [ -z "$bb" ]; then
-        read bb 0< lastip.conf
+    LOCALFILEIP=$(cat ./index.html | sed -e 's/\//\'$'\n/g' | grep -i 172 | sed -n '1p')
+    if [ -z "$LOCALFILEIP" ]; then
+        read LOCALFILEIP 0< lastip.conf
     fi
-    echo "本地文件的ip:$bb"
+    echo "$LOCALFILEIP"
     return 0;
 }
-getLocalFileIP
+LOCALFILEIP=$(getLocalFileIP)
+echo "本地文件的ip:$LOCALFILEIP"
 
-if [ -z "$aa" ] || [ -z "$bb" ] || [ "$aa" == "$bb" ]; then
+#echo $NETIP
+
+if [ -z "$NETIP" ] || [ -z "$LOCALFILEIP" ] || [ "$NETIP" == "$LOCALFILEIP" ]; then
     echo "替换失败!获取到的ip无效或二者相同"
     exit 0
 fi
 
 # 调用update.sh脚本执行替换操作
-./update.sh $bb $aa html
-./update.sh $bb $aa plist
+./update.sh $LOCALFILEIP $NETIP html
+./update.sh $LOCALFILEIP $NETIP plist
 
 # 替换完成,记录上一次的ip
-echo "$aa" 1> lastip.conf
+echo "$NETIP" 1> lastip.conf
 
 # 输出成功提示
-URL="http://$aa"
+URL="http://$NETIP"
 echo -e "🍺 浏览器访问: \033[34m$URL\033[0m"
 
 # 生成访问本站地址的二维码
